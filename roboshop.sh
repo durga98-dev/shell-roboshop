6b9543bd3c550a5816630e20586f6ac1fb36c207
Durga <durgadevi0707@gmail.com>
2025-12-29 22:41:43 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,7 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0093c1082e71258cf"
 DOMAIN_NAME="durgadevops.fun"
+ZONE_ID="Z0824629209JSDF9VAIXI"
 
 for instance in $@
 do
@@ -13,6 +14,27 @@ do
         RECORD_NAME="$instance.$DOMAIN_NAME)
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
+
+    echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
