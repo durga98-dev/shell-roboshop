c611321eb4a032781af338793f1dbf692467057f
Durga <durgadevi0707@gmail.com>
2025-12-29 22:44:27 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -11,7 +11,7 @@ do
     #get private ip
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME)
+        RECORD_NAME="$instance.$DOMAIN_NAME)"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         RECORD_NAME="$DOMAIN_NAME"
